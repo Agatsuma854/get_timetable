45eaa9231756323b7aaf8fa9f1acf9333be8b25d
q55mehqs <eviyuyu14@gmail.com>
2019-04-17 08:32:30 +0900

    bot使用のために一部変更

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .class_schedule_manager import class_schedule_manager
--- class_schedule_manager.py
+++ class_schedule_manager.py
@@ -2,13 +2,32 @@ import datetime
 
 
 class class_schedule_manager():
+    """指定したクラスの時間割を取得します"""
     def __init__(self, csv_dir=''):
         self.csv_dir = csv_dir
         self.filename = ''
         self.period = 'first'
         self.make_table = {}
 
-    def get_schedule(self, myclass, day_of_the_week=0):
+    def get_schedule(self, myclass : str, day_of_the_week=0):
+        """ファイルの読み込みをし、使用しやすいような形式にした
+        ものを返す
+        Parameters
+        ----------
+        myclass : str
+            1学年は `1-1` 表記
+            上位学年は `IS3` など大文字で
+
+        day_of_the_week : int
+            0...今日分
+            その他の曜日指定は datetime.date.weekday() +1
+
+        Return
+        ------
+        dict
+            {1: ['地理', '矢澤', '教室'], 2: ['総合工学基礎', ...], ...}
+            など
+        """
         # config.csvファイルの読み込み
         with open('config.csv', newline='') as config:
 
@@ -17,9 +36,9 @@ class class_schedule_manager():
 
             for line in config:
                 # 学期の判定と決定
-                if (datetime.date.today().year == int(line[0]) and
-                        datetime.date.today().month <= int(line[1]) and
-                        datetime.date.today().day <= int(line[2])):
+                if (datetime.date.today().year == int(line[0])
+                        and datetime.date.today().month <= int(line[1])
+                        and datetime.date.today().day <= int(line[2])):
                     self.period = 'second'
 
             # 読み込むファイルの名前の生成
@@ -53,12 +72,26 @@ class class_schedule_manager():
         return self.make_table
 
     # 指定したCSVファイルを読み出し
-    def _read_csv(self, row_num):
-        with open(self.filename, newline='') as timetable:
-            for i, list in enumerate(timetable):
+    def _read_csv(self, row_num : int):
+        """
+        Parameter
+        ---------
+        row_num : int
+            csvファイルの行が曜日を示しているので、
+            引数で行の数を指定することで曜日を示す
+            1: 月、 2: 火, ...
+
+        Return
+        ------
+        list
+            ['地理', '矢澤', '教室'] など
+            ファイルのコンマで要素を分けてリスト化したものを返す
+        """
+        with open(self.filename, newline='', encoding="utf-8") as timetable:
+            for i, row in enumerate(timetable):
                 if i == row_num:
                     # ここのでーたの状態 [e:b:c, ]
-                    return list.split(',')
+                    return row.split(',')
         return [
             "Invalid date::", "Invalid date::",
             "Invalid date::", "Invalid date::"
